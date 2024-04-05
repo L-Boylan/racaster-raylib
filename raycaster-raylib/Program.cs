aff13e987a5f8a4f03f8e693fa736a919cbfa2c5
Levi Boylan <boylan_levi@hotmail.co.uk>
2024-04-05 21:38:32 +0100

    Added simple states system; mainmenu/pause/gameplay

--- raycaster-raylib/Program.cs
+++ raycaster-raylib/Program.cs
@@ -11,6 +11,13 @@ namespace raycaster_raylib
         private const int ScreenHeight = 1080;
         private const int GridSpacing = 21;
         
+        private enum GameState
+        {
+            MainMenu,
+            FirstPerson,
+            Pause
+        }
+        
         static void Main()
         {
             int[,] worldMap = 
@@ -49,6 +56,8 @@ namespace raycaster_raylib
             var planeY = 0.66;
 
             var drawMap = false;
+
+            var currentScreen = GameState.MainMenu;
             
             Raylib.InitWindow(1920, 1080, "Shallom");
             
@@ -56,182 +65,218 @@ namespace raycaster_raylib
 
             while (!Raylib.WindowShouldClose())
             {
+                switch (currentScreen)
+                {
+                    case GameState.MainMenu:
+                        if (Raylib.IsKeyPressed(KeyboardKey.Enter))
+                        {
+                            currentScreen = GameState.FirstPerson;
+                        }
+                        break;
+                    case GameState.FirstPerson:
+                        if (Raylib.IsKeyPressed(KeyboardKey.Tab))
+                        {
+                            currentScreen = GameState.Pause;
+                        }
+                        break;
+                    case GameState.Pause:
+                        if (Raylib.IsKeyPressed(KeyboardKey.Tab))
+                        {
+                            currentScreen = GameState.FirstPerson;
+                        }
+                        break;
+                }
+                
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.Blank);
 
-                for (int x = 0; x < ScreenWidth; x++)
+                switch (currentScreen)
                 {
-                    //ray position and direction
-                    var cameraX = 2 * x / (double)ScreenWidth - 1;
-                    var rayDirX = dirX + planeX * cameraX;
-                    var rayDirY = dirY + planeY * cameraX;
-                    
-                    //what map box we're in
-                    var mapX = (int)posX;
-                    var mapY = (int)posY;
-
-                    //ray length from current position to next x or y side
-                    double sideDistX;
-                    double sideDistY;
-                    
-                    //length of ray from one x or y side to next x or y side
-                    var deltaDistX = rayDirX == 0 ? 1e30 : Math.Abs(1 / rayDirX);
-                    var deltaDistY = rayDirY == 0 ? 1e30 : Math.Abs(1 / rayDirY);
-
-                    double perpWallDist;
-
-                    int stepX;
-                    int stepY;
-
-                    var hit = 0;
-                    var side = 0;
-
-                    if (rayDirX < 0)
-                    {
-                        stepX = -1;
-                        sideDistX = (posX - mapX) * deltaDistX;
-                    }
-                    else
-                    {
-                        stepX = 1;
-                        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-                    }
-
-                    if (rayDirY < 0)
-                    {
-                        stepY = -1;
-                        sideDistY = (posY - mapY) * deltaDistY;
-                    }
-                    else
-                    {
-                        stepY = 1;
-                        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-                    }
-
-                    while (hit == 0)
-                    {
-                        if (sideDistX < sideDistY)
+                    case GameState.MainMenu:
+                        Raylib.ClearBackground(Color.SkyBlue);
+                        Raylib.DrawText("MAIN MENU", (ScreenWidth / 2 - 100), (ScreenHeight / 2 - 20), 40, Color.Black);
+                        Raylib.DrawText("press enter to start", (ScreenWidth / 2 - 100), (ScreenHeight / 2 + 40), 20, Color.Black);
+                        break;
+                    case GameState.FirstPerson: 
+                        Raylib.ClearBackground(Color.Blank);
+        
+                        for (int x = 0; x < ScreenWidth; x++)
                         {
-                            sideDistX += deltaDistX;
-                            mapX += stepX;
-                            side = 0;
+                            //ray position and direction
+                            var cameraX = 2 * x / (double)ScreenWidth - 1;
+                            var rayDirX = dirX + planeX * cameraX;
+                            var rayDirY = dirY + planeY * cameraX;
+                            
+                            //what map box we're in
+                            var mapX = (int)posX;
+                            var mapY = (int)posY;
+        
+                            //ray length from current position to next x or y side
+                            double sideDistX;
+                            double sideDistY;
+                            
+                            //length of ray from one x or y side to next x or y side
+                            var deltaDistX = rayDirX == 0 ? 1e30 : Math.Abs(1 / rayDirX);
+                            var deltaDistY = rayDirY == 0 ? 1e30 : Math.Abs(1 / rayDirY);
+        
+                            double perpWallDist;
+        
+                            int stepX;
+                            int stepY;
+        
+                            var hit = 0;
+                            var side = 0;
+        
+                            if (rayDirX < 0)
+                            {
+                                stepX = -1;
+                                sideDistX = (posX - mapX) * deltaDistX;
+                            }
+                            else
+                            {
+                                stepX = 1;
+                                sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+                            }
+        
+                            if (rayDirY < 0)
+                            {
+                                stepY = -1;
+                                sideDistY = (posY - mapY) * deltaDistY;
+                            }
+                            else
+                            {
+                                stepY = 1;
+                                sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+                            }
+        
+                            while (hit == 0)
+                            {
+                                if (sideDistX < sideDistY)
+                                {
+                                    sideDistX += deltaDistX;
+                                    mapX += stepX;
+                                    side = 0;
+                                }
+                                else
+                                {
+                                    sideDistY += deltaDistY;
+                                    mapY += stepY;
+                                    side = 1;
+                                }
+        
+                                if (worldMap[mapX, mapY] > 0)
+                                    hit = 1;
+                            }
+        
+                            if (side == 0)
+                            {
+                                perpWallDist = (sideDistX - deltaDistX);
+                            }
+                            else
+                            {
+                                perpWallDist = (sideDistY - deltaDistY);
+                            }
+        
+                            var lineHeight = (int)(ScreenHeight / perpWallDist);
+        
+                            var drawStart = -lineHeight / 2 + ScreenHeight / 2;
+                            if (drawStart < 0) drawStart = 0;
+                            var drawEnd = lineHeight / 2 + ScreenHeight / 2;
+                            if (drawEnd >= ScreenHeight) drawEnd = ScreenHeight - 1;
+        
+                            Color colour;
+                            switch (worldMap[mapX, mapY])
+                            {
+                                case 1:
+                                    colour = Color.Red;
+                                    break;
+                                case 2:
+                                    colour = Color.Green;
+                                    break;
+                                case 3:
+                                    colour = Color.Blue;
+                                    break;
+                                case 4:
+                                    colour = Color.White;
+                                    break;
+                                default:
+                                    colour = Color.Yellow;
+                                    break;
+                            }
+        
+                            //give x and y side a different brightness
+                            //if(side == 1) {color = color / 2;}
+                            
+                            // Draw Walls
+                            Raylib.DrawLine(x, drawStart, x, drawEnd, colour);
+                            // Draw Ceiling
+                            Raylib.DrawLine(x, 0, x, drawStart, Color.Gold);
+                            //Draw Floor
+                            Raylib.DrawLine(x, ScreenHeight, x, drawEnd, Color.DarkGray);
                         }
-                        else
+        
+                        if (drawMap)
                         {
-                            sideDistY += deltaDistY;
-                            mapY += stepY;
-                            side = 1;
+                            DrawMap(worldMap, posX, posY, dirX, dirY);
                         }
-
-                        if (worldMap[mapX, mapY] > 0)
-                            hit = 1;
-                    }
-
-                    if (side == 0)
-                    {
-                        perpWallDist = (sideDistX - deltaDistX);
-                    }
-                    else
-                    {
-                        perpWallDist = (sideDistY - deltaDistY);
-                    }
-
-                    var lineHeight = (int)(ScreenHeight / perpWallDist);
-
-                    var drawStart = -lineHeight / 2 + ScreenHeight / 2;
-                    if (drawStart < 0) drawStart = 0;
-                    var drawEnd = lineHeight / 2 + ScreenHeight / 2;
-                    if (drawEnd >= ScreenHeight) drawEnd = ScreenHeight - 1;
-
-                    Color colour;
-                    switch (worldMap[mapX, mapY])
-                    {
-                        case 1:
-                            colour = Color.Red;
-                            break;
-                        case 2:
-                            colour = Color.Green;
-                            break;
-                        case 3:
-                            colour = Color.Blue;
-                            break;
-                        case 4:
-                            colour = Color.White;
-                            break;
-                        default:
-                            colour = Color.Yellow;
-                            break;
-                    }
-
-                    //give x and y side a different brightness
-                    //if(side == 1) {color = color / 2;}
-                    
-                    // Draw Walls
-                    Raylib.DrawLine(x, drawStart, x, drawEnd, colour);
-                    // Draw Ceiling
-                    Raylib.DrawLine(x, 0, x, drawStart, Color.Gold);
-                    //Draw Floor
-                    Raylib.DrawLine(x, ScreenHeight, x, drawEnd, Color.DarkGray);
+                        
+                        
+                        var frameTime = Raylib.GetFrameTime();
+        
+                        var moveSpeed = frameTime * 5.0;
+                        var stepSpeed = 1;
+                        var rotSpeed = frameTime * 3.0;
+                        var stepTurn = Math.PI / 2;
+        
+                        if (Raylib.IsKeyPressed(KeyboardKey.W) || Raylib.IsKeyPressed(KeyboardKey.Up))
+                        {
+                            var resultX = (int)(posX + dirX * stepSpeed);
+                            var resultY = (int)(posY + dirY * stepSpeed);
+                            
+                            if (worldMap[resultX, (int)posY] == 0) posX += dirX * stepSpeed;
+                            if (worldMap[(int)posX, resultY] == 0) posY += dirY * stepSpeed;
+                        }
+                        if (Raylib.IsKeyPressed(KeyboardKey.S) || Raylib.IsKeyPressed(KeyboardKey.Down))
+                        {
+                            var resultX = (int)(posX - dirX * stepSpeed);
+                            var resultY = (int)(posY - dirY * stepSpeed);
+                            
+                            if (worldMap[resultX, (int)posY] == 0) posX -= dirX * stepSpeed;
+                            if (worldMap[(int)posX, resultY] == 0) posY -= dirY * stepSpeed;
+                        }
+        
+                        if (Raylib.IsKeyPressed(KeyboardKey.D) || Raylib.IsKeyPressed(KeyboardKey.Right))
+                        {
+                            var oldDirX = dirX;
+                            var oldPlaneX = planeX;
+        
+                            dirX = dirX * Math.Cos(-stepTurn) - dirY * Math.Sin(-stepTurn);
+                            dirY = oldDirX * Math.Sin(-stepTurn) + dirY * Math.Cos(-stepTurn);
+        
+                            planeX = planeX * Math.Cos(-stepTurn) - planeY * Math.Sin(-stepTurn);
+                            planeY = oldPlaneX * Math.Sin(-stepTurn) + planeY * Math.Cos(-stepTurn);
+                        }
+                        if (Raylib.IsKeyPressed(KeyboardKey.A) || Raylib.IsKeyPressed(KeyboardKey.Left))
+                        {
+                            var oldDirX = dirX;
+                            var oldPlaneX = planeX;
+                            
+                            dirX = dirX * Math.Cos(stepTurn) - dirY * Math.Sin(stepTurn);
+                            dirY = oldDirX * Math.Sin(stepTurn) + dirY * Math.Cos(stepTurn);
+                            
+                            planeX = planeX * Math.Cos(stepTurn) - planeY * Math.Sin(stepTurn);
+                            planeY = oldPlaneX * Math.Sin(stepTurn) + planeY * Math.Cos(stepTurn);
+                        }
+        
+                        if (Raylib.IsKeyPressed(KeyboardKey.M)) drawMap = !drawMap;
+                        
+                        break;
+                    case GameState.Pause:
+                        Raylib.ClearBackground(Color.LightGray);
+                        Raylib.DrawText("PAUSED", 200, 200, 80, Color.Black);
+                        break;
                 }
-
-                if (drawMap)
-                {
-                    DrawMap(worldMap, posX, posY, dirX, dirY);
-                }
-                
-                
-                var frameTime = Raylib.GetFrameTime();
-
-                var moveSpeed = frameTime * 5.0;
-                var stepSpeed = 1;
-                var rotSpeed = frameTime * 3.0;
-                var stepTurn = Math.PI / 2;
-
-                if (Raylib.IsKeyPressed(KeyboardKey.W) || Raylib.IsKeyPressed(KeyboardKey.Up))
-                {
-                    var resultX = (int)(posX + dirX * stepSpeed);
-                    var resultY = (int)(posY + dirY * stepSpeed);
-                    
-                    if (worldMap[resultX, (int)posY] == 0) posX += dirX * stepSpeed;
-                    if (worldMap[(int)posX, resultY] == 0) posY += dirY * stepSpeed;
-                }
-                if (Raylib.IsKeyPressed(KeyboardKey.S) || Raylib.IsKeyPressed(KeyboardKey.Down))
-                {
-                    var resultX = (int)(posX - dirX * stepSpeed);
-                    var resultY = (int)(posY - dirY * stepSpeed);
-                    
-                    if (worldMap[resultX, (int)posY] == 0) posX -= dirX * stepSpeed;
-                    if (worldMap[(int)posX, resultY] == 0) posY -= dirY * stepSpeed;
-                }
-
-                if (Raylib.IsKeyPressed(KeyboardKey.D) || Raylib.IsKeyPressed(KeyboardKey.Right))
-                {
-                    var oldDirX = dirX;
-                    var oldPlaneX = planeX;
-
-                    dirX = dirX * Math.Cos(-stepTurn) - dirY * Math.Sin(-stepTurn);
-                    dirY = oldDirX * Math.Sin(-stepTurn) + dirY * Math.Cos(-stepTurn);
-
-                    planeX = planeX * Math.Cos(-stepTurn) - planeY * Math.Sin(-stepTurn);
-                    planeY = oldPlaneX * Math.Sin(-stepTurn) + planeY * Math.Cos(-stepTurn);
-                }
-
-                if (Raylib.IsKeyPressed(KeyboardKey.A) || Raylib.IsKeyPressed(KeyboardKey.Left))
-                {
-                    var oldDirX = dirX;
-                    var oldPlaneX = planeX;
-                    
-                    dirX = dirX * Math.Cos(stepTurn) - dirY * Math.Sin(stepTurn);
-                    dirY = oldDirX * Math.Sin(stepTurn) + dirY * Math.Cos(stepTurn);
-                    
-                    planeX = planeX * Math.Cos(stepTurn) - planeY * Math.Sin(stepTurn);
-                    planeY = oldPlaneX * Math.Sin(stepTurn) + planeY * Math.Cos(stepTurn);
-                }
-
-                if (Raylib.IsKeyPressed(KeyboardKey.M)) drawMap = !drawMap;
-                
+               
                 Raylib.DrawFPS(10, 10);
-                
                 Raylib.EndDrawing();
             }
             
